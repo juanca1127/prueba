e9072f9d2ea184de4f73c0aa23d87c61139fd4c5
juanca1127 <juancadelpatio@gmail.com>
2022-12-20 23:25:11 -0500

    correccion2

--- src/card/cardPrincipal.js
+++ src/card/cardPrincipal.js
@@ -5,19 +5,17 @@ const CardPrincipal =()=>{
  const [animacion,setAnimacion] = useState(" ")
  const [animacion2,setAnimacion2] = useState(" ")
     return(<>
-    <div style={{
-        border:"none"
-    }}  className="card bg-gradient text-center mt-4 card-zIndex">
+    <div  className=" bg-gradient text-center mt-4 ">
        
-        <div className="card-body">
+        <div  className="card-body mt-4">
             <div className="row">
-                <div className="col-6">
+                <div className="col-6 mt-4">
                 <h1 className="left ff animate__animated animate__bounce">Hi. This is Lorem.</h1>
                 <h4 className="left text-muted ff">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor in
                 </h4>
                 </div>
-                <div className="col-6">
+                <div className="col-6 mt-4">
                 <div className="btn-group-vertical" role="group" aria-label="Vertical button group">
         <button onMouseLeave={()=>setAnimacion(" ")} onMouseEnter={()=>setAnimacion("animate__animated animate__bounce animate__repeat-1")} type="button"  className={`btn btn-primary ff btn-lg d-flex justify-content-between` }>
             <div className={`text-bold ${animacion}`} > Ok lest's go  <i className="bi bi-arrow-right"></i>
